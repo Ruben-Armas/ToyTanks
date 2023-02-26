293cfc242b3c75468b8ab7363d1fff8a400dc3b1
Ruben-Armas <rubenarmas2010@gmail.com>
2023-02-26 15:05:30 +0000

    Start num of Players FIxed
    only worked when dropDown OnChange

--- Assets/Scripts/UI/DropdownPlayerHandler.cs
+++ Assets/Scripts/UI/DropdownPlayerHandler.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DropdownPlayerHandler : MonoBehaviour
 {
-    /*Dropdown NumOfPlayersdropdown;
-
-    public void OnNumOfPlayersDropdownChanged()
+    [SerializeField] private TMP_Dropdown dropdown;
+    //Guarda el valor tanto si se ha cambiado como si no
+    void Start()
     {
-        NumOfPlayersdropdown = GetComponent<Dropdown>();
-        int numPlayers = NumOfPlayersdropdown.value + 1;
+        int numPlayers = dropdown.value + 1;
+        Debug.Log($"Start players: {numPlayers}");
 
-        // Guardar el valor en PlayerPrefs  (para poder pasarloa a la otra escena)
+        // Guardar el valor inicial en PlayerPrefs
         PlayerPrefs.SetInt("numPlayers", numPlayers);
     }
-    */
-    private void NumOfPlayersChanged(int num)
+    public void OnPlayButtonClicked()
+    {
+        //Valor seleccionado
+        int numPlayers = dropdown.value + 1;
+        Debug.Log($"Players: {numPlayers}");
+
+        // Guardar el valor en PlayerPrefs  (para poder pasarlo a la otra escena)
+        PlayerPrefs.SetInt("numPlayers", numPlayers);
+    }
+
+    /*//Solo cuando cambia (en el OnChange del DropDown)
+    public void NumOfPlayersChanged(int num)
     {
         Debug.Log(num);
         int numPlayers = num + 1;
 
-        // Guardar el valor en PlayerPrefs  (para poder pasarloa a la otra escena)
+        // Guardar el valor en PlayerPrefs  (para poder pasarlo a la otra escena)
         PlayerPrefs.SetInt("numPlayers", numPlayers);
-    }
+    }*/
 }
